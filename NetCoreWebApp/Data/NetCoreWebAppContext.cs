78a07b254b887b60214bbd70fb786268cf2bc472
Kushan Hansika <KushanH@99x.lk>
2019-08-23 16:10:48 +0530

    Add project files.

---
+++ NetCoreWebApp/Data/NetCoreWebAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace NetCoreWebApp.Models
+{
+    public class NetCoreWebAppContext : DbContext
+    {
+        public NetCoreWebAppContext (DbContextOptions<NetCoreWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<NetCoreWebApp.Models.Movie> Movie { get; set; }
+    }
+}
